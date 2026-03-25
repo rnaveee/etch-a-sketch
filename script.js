1902aacccf7104c35eabe74823b31cc16edecba4
Ryan Nave <ryannave97@gmail.com>
2026-03-25 13:37:36 -0700

    finished sketch logic

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const gridContainer = document.querySelector("#grid-container");
 
-for(let r = 0; r < 16; r++){
+let boxes = [];
+let mouseState = false;
+document.addEventListener("mousedown", () => {
+    mouseState = true;
+});
+document.addEventListener("mouseup", () => {
+    mouseState = false;
+});
+
+for(let r = 0; r < 50; r++){
     const gridContainerRow = document.createElement("div");
     gridContainerRow.style.display = "flex";
-    for(let i = 0; i < 100; i++){
+    for(let i = 0; i < 50; i++){
         const newDiv = document.createElement("div");
+        boxes.push(newDiv);
         newDiv.style.height = "10px";
         newDiv.style.width = "10px";
         newDiv.style.border = "1px solid black";
+        newDiv.addEventListener("mouseout", () => {
+            if(!newDiv.classList.contains("clicked")){
+                newDiv.style.backgroundColor = "white";
+            }
+        });
+        newDiv.addEventListener("mouseover", () => {
+            if(!newDiv.classList.contains("clicked") && !mouseState){
+                newDiv.style.backgroundColor = "gray";
+            } else if (!newDiv.classList.contains("clicked") && mouseState) {
+                newDiv.classList.add("clicked");
+                newDiv.style.backgroundColor = "black";
+            }
+        });
         gridContainerRow.appendChild(newDiv);
     }
     gridContainer.appendChild(gridContainerRow);
