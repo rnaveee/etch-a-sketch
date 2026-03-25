637c42ad16f297c1b8ccbcee6edf44ae6df22101
Ryan Nave <ryannave97@gmail.com>
2026-03-25 12:23:14 -0700

    implemented grid logic

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const gridContainer = document.querySelector("#grid-container");
 
-for(let i = 0; i < 100; i++){
-    const newDiv = document.createElement("div");
-    newDiv.style.display = "flex";
-    newDiv.style.height = "10px";
-    newDiv.style.width = "10px";
-    newDiv.style.border = "1px solid black";
-    gridContainer.appendChild(newDiv);
+for(let r = 0; r < 16; r++){
+    const gridContainerRow = document.createElement("div");
+    gridContainerRow.style.display = "flex";
+    for(let i = 0; i < 100; i++){
+        const newDiv = document.createElement("div");
+        newDiv.style.height = "10px";
+        newDiv.style.width = "10px";
+        newDiv.style.border = "1px solid black";
+        gridContainerRow.appendChild(newDiv);
+    }
+    gridContainer.appendChild(gridContainerRow);
 }
-const newDiv = document.createElement("div");
-newDiv.style.height = "10px";
-newDiv.style.width = "10px";
-newDiv.style.border = "1px solid black";
-gridContainer.appendChild(newDiv);
