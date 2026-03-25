20c95c67c3ca5d2e62008265f927787cfea1b709
Ryan Nave <ryannave97@gmail.com>
2026-03-25 14:11:41 -0700

    finished etchasketch

--- script.js
+++ script.js
@@ -9,29 +9,50 @@ document.addEventListener("mouseup", () => {
     mouseState = false;
 });
 
-for(let r = 0; r < 50; r++){
-    const gridContainerRow = document.createElement("div");
-    gridContainerRow.style.display = "flex";
-    for(let i = 0; i < 50; i++){
-        const newDiv = document.createElement("div");
-        boxes.push(newDiv);
-        newDiv.style.height = "10px";
-        newDiv.style.width = "10px";
-        newDiv.style.border = "1px solid black";
-        newDiv.addEventListener("mouseout", () => {
-            if(!newDiv.classList.contains("clicked")){
-                newDiv.style.backgroundColor = "white";
-            }
-        });
-        newDiv.addEventListener("mouseover", () => {
-            if(!newDiv.classList.contains("clicked") && !mouseState){
-                newDiv.style.backgroundColor = "gray";
-            } else if (!newDiv.classList.contains("clicked") && mouseState) {
-                newDiv.classList.add("clicked");
-                newDiv.style.backgroundColor = "black";
-            }
-        });
-        gridContainerRow.appendChild(newDiv);
+let length;
+let width;
+const lengthField = document.getElementById("length");
+const widthField = document.getElementById("width");
+const createButton = document.getElementById("create");
+
+createButton.addEventListener("click", () => {
+    gridContainer.innerHTML = "";
+    length = lengthField.value;
+    width = widthField.value;
+    createSketch(length, width);
+});
+
+function createSketch(length, width){
+    for(let r = 0; r < length; r++){
+        const gridContainerRow = document.createElement("div");
+        gridContainerRow.style.display = "flex";
+        for(let i = 0; i < width; i++){
+            const newDiv = document.createElement("div");
+            boxes.push(newDiv);
+            newDiv.style.height = "10px";
+            newDiv.style.width = "10px";
+            newDiv.style.border = "1px solid black";
+            newDiv.addEventListener("mouseout", () => {
+                if(!newDiv.classList.contains("clicked")){
+                    newDiv.style.backgroundColor = "white";
+                }
+            });
+            newDiv.addEventListener("mouseover", () => {
+                if(!newDiv.classList.contains("clicked") && !mouseState){
+                    newDiv.style.backgroundColor = "gray";
+                } else if (!newDiv.classList.contains("clicked") && mouseState) {
+                    newDiv.classList.add("clicked");
+                    newDiv.style.backgroundColor = "black";
+                }
+            });
+            newDiv.addEventListener("click", () => {
+                if(!newDiv.classList.contains("clicked")){
+                    newDiv.classList.add("clicked");
+                    newDiv.style.backgroundColor = "black";
+                }
+            });
+            gridContainerRow.appendChild(newDiv);
+        }
+        gridContainer.appendChild(gridContainerRow);
     }
-    gridContainer.appendChild(gridContainerRow);
 }
